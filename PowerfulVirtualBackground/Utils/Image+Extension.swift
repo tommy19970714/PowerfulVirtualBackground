dcab4c638da321d4301021964fd75e7b2885ce35
toshiki tomihira <toshiki19970714@gmail.com>
2021-09-19 01:46:26 +0900

    Add pixelBuffer extension

--- PowerfulVirtualBackground/Utils/Image+Extension.swift
+++ PowerfulVirtualBackground/Utils/Image+Extension.swift
@@ -23,6 +23,46 @@ extension NSImage {
         newImage.size = destSize
         return NSImage(data: newImage.tiffRepresentation!)!
     }
+    
+    func pixelBuffer() -> CVPixelBuffer? {
+           let width = self.size.width
+           let height = self.size.height
+           let attrs = [kCVPixelBufferCGImageCompatibilityKey: kCFBooleanTrue,
+                        kCVPixelBufferCGBitmapContextCompatibilityKey: kCFBooleanTrue] as CFDictionary
+           var pixelBuffer: CVPixelBuffer?
+           let status = CVPixelBufferCreate(kCFAllocatorDefault,
+                                            Int(width),
+                                            Int(height),
+                                            kCVPixelFormatType_32ARGB,
+                                            attrs,
+                                            &pixelBuffer)
+           
+           guard let resultPixelBuffer = pixelBuffer, status == kCVReturnSuccess else {
+               return nil
+           }
+           
+           CVPixelBufferLockBaseAddress(resultPixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
+           let pixelData = CVPixelBufferGetBaseAddress(resultPixelBuffer)
+           
+           let rgbColorSpace = CGColorSpaceCreateDeviceRGB()
+           guard let context = CGContext(data: pixelData,
+                                         width: Int(width),
+                                         height: Int(height),
+                                         bitsPerComponent: 8,
+                                         bytesPerRow: CVPixelBufferGetBytesPerRow(resultPixelBuffer),
+                                         space: rgbColorSpace,
+                                         bitmapInfo: CGImageAlphaInfo.noneSkipFirst.rawValue) else {return nil}
+           
+           let graphicsContext = NSGraphicsContext(cgContext: context, flipped: false)
+           NSGraphicsContext.saveGraphicsState()
+           NSGraphicsContext.current = graphicsContext
+           draw(in: CGRect(x: 0, y: 0, width: width, height: height))
+           NSGraphicsContext.restoreGraphicsState()
+           
+           CVPixelBufferUnlockBaseAddress(resultPixelBuffer, CVPixelBufferLockFlags(rawValue: 0))
+           
+           return resultPixelBuffer
+       }
 }
 
 extension CIImage {
